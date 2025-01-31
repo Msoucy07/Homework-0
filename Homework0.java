e40303f186ca67ad88b7688e6ef9e1db61bb69fb
Msoucy07 <matthew.soucy1@maine.edu>
2025-01-31 11:02:51 -0500

    Create Homework0.java

---
+++ Homework0.java
@@ -0,0 +1,9 @@
+public class Homework0{
+
+    public static void main(String[] args) {
+        System.out.println("Hello Wrorld");
+
+        System.out.println("Matthew Soucy");
+        
+    }
+}
